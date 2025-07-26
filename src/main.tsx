f88a43d4b8df356634c38319018c2969c00cc92a
G Veera Sekhar <complanboy2@gmail.com>
2025-07-26 14:19:27 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/audio-oracle-speaks">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
